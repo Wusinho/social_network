5b62a80401695cbb9f75d1632820d39247319e17
Heber Lazo <heberlazo@gmail.com>
2022-12-23 19:53:01 -0500

    invitation model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,26 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :todos
+  has_many :invitations
+  has_many :pending_invitations, -> { where confirmed: false }, class_name: 'Invitation', foreign_key: 'friend_id'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def friends
+    friends_i_sent_invitation = Invitation.where(user_id: id, confirmed: true).pluck(:friend_id)
+    friends_i_got_invitation = Invitation.where(friend_id: id, confirmed: true).pluck(:user_id)
+    ids = friends_i_got_invitation + friends_i_sent_invitation
+    User.where(id: ids)
+  end
+
+  def friend_with?(user)
+    Invitation.confirmed_record?(id, user.id)
+  end
+
+  def send_invitation(user)
+    Invitation.create(friend_id: user.id)
+  end
+
 end
