9c0b23fb393884c1eb816d83a52ef55bcd7ce7f2
Heber Lazo <heberlazo@gmail.com>
2022-12-25 13:58:07 -0500

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+users = %i[ Omar Heber Joao Kelly Maria Antonio]
+
+users.each do |user|
+  User.create(username: user, email: "#{user}@gmail.com", password: 123456, password_confirmation: 123456)
+end
+
+todo = 'First message'
+
+users= User.all
+
+users.each do |user|
+  Todo.create(user_id: user.id, body: todo)
+end
